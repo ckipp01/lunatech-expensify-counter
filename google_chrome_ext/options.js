eb9dff3236ba3661dcafc3f324b281b2e1126806
Nataliia Dziubenko <nataliia.dziubenko@lunatech.com>
2018-09-25 10:23:34 +0200

    amount value can be set to double value

--- google_chrome_ext/options.js
+++ google_chrome_ext/options.js
@@ -3,7 +3,7 @@ let limitInput = document.getElementById('limit-input');
 let saveButton = document.getElementById('save');
 
 saveButton.onclick = function(element) {
-  if (limitInput.value && /^-*[0-9]*$/.test(limitInput.value)) {
+  if (limitInput.value && /^-*[0-9]*[.]*[0-9]*$/.test(limitInput.value)) {
     chrome.storage.sync.set({annualAmount: limitInput.value});
   } else {
     alert("invalid value, amount was not changed");
